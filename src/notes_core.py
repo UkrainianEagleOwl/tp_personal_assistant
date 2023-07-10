65a8b94684afc33c0a4c4b2878c62a6d23518967
paukdv <paukdv@gmail.com>
2023-07-10 19:47:43 +0300

    added print notes

--- src/notes_core.py
+++ src/notes_core.py
@@ -1,10 +1,13 @@
 import json
+from prettytable import PrettyTable
+from colorama import init, Fore, Style
 
 
 class Tag:
     def __init__(self, name):
         self.name = name
-   
+
+
 class Note:
     def __init__(self, title, tags, description):
         self.title = title
@@ -15,17 +18,18 @@ class Note:
         else:
             self.tags = Tag(tags) if isinstance(tags, str) else tags
         self.description = description
-   
+
     def __repr__(self):
         tags_str = ""
         if self.tags:
             tags_str = ", ".join(tag.name for tag in self.tags)
         return "Title: {}\nTags: {}\nDescription: {}".format(self.title, tags_str, self.description)
-       
+
     def to_dict(self):
         notes_data = []
         for note in self.notes:
-            tags_data = [tag.name for tag in note.tags]  # Сохраняем только имена тегов
+            # Сохраняем только имена тегов
+            tags_data = [tag.name for tag in note.tags]
             notes_data.append({
                 'title': note.title,
                 'tags': tags_data,  # Используем сохраненные имена тегов
@@ -33,14 +37,15 @@ class Note:
             })
         return {'notes': notes_data}
 
+
 class Notebook:
     def __init__(self):
         self.notes = []
 
-    def add_note(self, note):                              
+    def add_note(self, note):
         self.notes.append(note)
-        
-    def remove_note(self, title):                          
+
+    def remove_note(self, title):
         matching_notes = []
         for note in self.notes:
             if note.title.lower() == title.lower():
@@ -48,39 +53,47 @@ class Notebook:
         for matching_note in matching_notes:
             self.notes.remove(matching_note)
 
- 
     def show_notes(self):
         if not self.notes:
             print("У нотатника нет записей.")
         else:
             print("Заметки в нотатнике:")
+            table = PrettyTable()
+            table.field_names = ['№', 'Title', 'Tags', 'Description']
+            counter = 1
             for note in self.notes:
-                print("Название:", note.title)
-                print("Теги:")
-                for tag in note.tags:
-                    if isinstance(tag, Tag):
-                        print("- ", tag.name)
-                    else:
-                        print("- ", tag)
-                print("Описание:", note.description)
-                print()
+                tags = [Fore.CYAN + '#' + tag +
+                        Style.RESET_ALL for tag in note['tags']]
+                table.add_row([counter, note['title'],
+                              ', '.join(tags), note['description']])
+                counter += 1
+        print(table)
 
-                
     def search_notes_by_tag(self, tag_name):
         matching_notes = []
         for note in self.notes:
             for tag in note.tags:
                 if tag.name.lower() == tag_name.lower():
                     matching_notes.append(note)
-                    #Stop check, if tag in found in note
+                    # Stop check, if tag in found in note
                     break
-        return matching_notes
-    
+        table = PrettyTable()
+        table.field_names = ['№', 'Title', 'Tags', 'Description']
+        counter = 1
+        for note in matching_notes:
+            tags = [Fore.CYAN + '#' + tag +
+                    Style.RESET_ALL for tag in note['tags']]
+            table.add_row([counter, note['title'],
+                           ', '.join(tags), note['description']])
+            counter += 1
+        print(table)
+
     def sort_notes_by_tag(self):
-        self.notes.sort(key=lambda note: [tag.name.lower() for tag in note.tags])
+        self.notes.sort(key=lambda note: [
+                        tag.name.lower() for tag in note.tags])
 
+    # добавляем методы сохр/загрузки json и методы преобразования в словарь
 
-    # добавляем методы сохр/загрузки json и методы преобразования в словарь 
     def save_json(self, filename):
         with open(filename, 'w') as file:
             json.dump(self.to_dict(), file, indent=4)
@@ -92,7 +105,8 @@ class Notebook:
         notebook = cls()
         for note_data in data['notes']:
             title = note_data['title']
-            tags = [Tag(tag_name) for tag_name in note_data['tags']]  # Создаем объекты Teg
+            tags = [Tag(tag_name)
+                    for tag_name in note_data['tags']]  # Создаем объекты Teg
             description = note_data['description']
             note = Note(title, tags, description)
             notebook.add_note(note)
@@ -101,21 +115,17 @@ class Notebook:
     def to_dict(self):
         notes_data = []
         for note in self.notes:
-            tags_data = [tag.name for tag in note.tags]  # Сохраняем только имена тегов
+            # Сохраняем только имена тегов
+            tags_data = [tag.name for tag in note.tags]
             notes_data.append({
                 'title': note.title,
                 'tags': tags_data,  # Используем сохраненные имена тегов
                 'description': note.description
             })
         return {'notes': notes_data}
-    
+
     def default(self, obj):
         if isinstance(obj, Tag):
             return obj.name
-        raise TypeError(f'Object of type {obj.__class__.__name__} is not JSON serializable')
-   
-
-   
-
-
-
+        raise TypeError(
+            f'Object of type {obj.__class__.__name__} is not JSON serializable')
