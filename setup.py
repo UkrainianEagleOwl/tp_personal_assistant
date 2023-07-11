f37f96bdeffdad9b34cc7488100795d4ac4f874b
UkrainianEagleOwl <127621332+UkrainianEagleOwl@users.noreply.github.com>
2023-07-11 16:13:41 +0300

    add new project important files

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Jarvis',
-    version='0.3',
+    name='jarvis_assistant_bot',
+    version='0.4',
     description='Personal assistant bot',
     author='Python Forces',
     url='https://github.com/UkrainianEagleOwl/tp_personal_assistant/tree/97c820e0779d54e488d5d824cce404b06bb4e654',
