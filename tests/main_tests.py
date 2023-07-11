2be26578e6fb04410f1d0666559f55b741f1ad69
UkrainianEagleOwl <127621332+UkrainianEagleOwl@users.noreply.github.com>
2023-07-12 01:12:40 +0300

    Tests realisation

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,2 +1,25 @@
-from tests.memory_tests import*
-from tests.save_load_tests import *
+from memory_tests import*
+from save_load_tests import *
+from notes_core_tests import *
+
+if __name__ == '__main__':
+    # Create a test suite
+    suite = unittest.TestSuite()
+
+    # Add test cases from each test file to the suite
+    # notes tests
+    suite.addTest(unittest.makeSuite(NotebookTestCase))
+    suite.addTest(unittest.makeSuite(NoteTestCase))
+    suite.addTest(unittest.makeSuite(TagTestCase))
+    #save load tests
+    suite.addTest(unittest.makeSuite(BooksSaveLoadTests))
+    #memory tests
+    suite.addTest(unittest.makeSuite(AddressBookTest))
+    suite.addTest(unittest.makeSuite(NameTest))
+    suite.addTest(unittest.makeSuite(PhoneTest))
+    suite.addTest(unittest.makeSuite(BirthdayTestCase))
+    suite.addTest(unittest.makeSuite(RecordTestCase))
+
+    # Create a test runner and run the suite
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite)
