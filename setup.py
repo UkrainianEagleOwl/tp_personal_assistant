83ae3ed45b2ce19b948085c9f456ad306379e633
UkrainianEagleOwl <127621332+UkrainianEagleOwl@users.noreply.github.com>
2023-07-12 23:52:27 +0300

    remake structure of game in project folder

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='jarvis_assistant_bot',
-    version='1.0',
-    description='Personal assistant bot',
+    version='1.001',
+    description='Welcome to Jarvis, your personal assistant! Jarvis is here to help you stay organized and manage your contacts, reminders, notes, and files efficiently.',
     author='Python Forces',
     url='https://github.com/UkrainianEagleOwl/tp_personal_assistant/tree/97c820e0779d54e488d5d824cce404b06bb4e654',
-    packages=['src','game'],
+    packages=['src'],
     
     entry_points={
         'console_scripts': [
